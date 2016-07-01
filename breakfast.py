b392d24430e994a52dcf43e88e87de98fc193197
David Gouldsmith <gouldsmith@users.noreply.github.com>
2016-07-01 13:54:14 -0400

    Add files via upload

---
+++ breakfast.py
@@ -0,0 +1,19 @@
+""" A functional breakfast """
+
+def mix_and_cook():
+	print('Mixing the ingredients')
+	print('Greasing the frying pan')
+	print('Pouring the mixture int a frying pan')
+	print('Cooking the first side')
+	print('Cooking the other side')
+	
+def make_omelette(ingredient):
+	mix_and_cook()
+	omelette = 'a tasty {} omelette'.format(ingredient)
+	return omelette
+	
+def make_pancake():
+	mix_and_cook()
+	omelette = 'a tasty omelette'
+	return pancake
+	
